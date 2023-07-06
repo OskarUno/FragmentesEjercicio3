59ee40853c7c603ab2b1d2c139b3cf86a10defd2
OskarUno <oorellana80@gmail.com>
2023-07-05 21:11:52 -0400

    Ejercicio Individual 3, se crean los 3 fragmentos y se agrega libreria de navegacion

--- app/src/main/java/com/awakelab/oskar/fragmentesejercicio3/FragmentLogin.java
+++ app/src/main/java/com/awakelab/oskar/fragmentesejercicio3/FragmentLogin.java
@@ -28,24 +28,6 @@ public class FragmentLogin extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentLogin.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentLogin newInstance(String param1, String param2) {
-        FragmentLogin fragment = new FragmentLogin();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
